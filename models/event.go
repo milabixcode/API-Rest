67210fabbf01d9f144c3cd5b0c29446bdd75bf85
camila.ferrari <camila.ferrari@bemobi.com>
2024-08-14 16:48:05 -0300

    Storing data in the Database (insert)

--- models/event.go
+++ models/event.go
@@ -1,22 +1,39 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	"API.com/api-test/db"
+)
 
 type Event struct {
-	ID          int       `binding: "required"`
+	ID          int64     `binding: "required"`
 	Name        string    `binding: "required"`
 	Description string    `binding: "required"`
 	Location    string    `binding: "required"`
 	DateTime    time.Time `binding: "required"`
-	UserID      int       
+	UserID      int
 }
 
 var events = []Event{}
 
 // Método que pertence ao tipo Event
-func (e Event) Save() {
-	
-	events = append(events, e)
+func (e Event) Save() error {
+	query := `
+	INSERT INTO events(name, descrption, location, dateTime, user_id)
+	VALUES(?, ?, ?, ?, ?)`
+	stmt, err := db.DB.Prepare(query)
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+	result, err := stmt.Exec(e.Name, e.Description, e.Location, e.DateTime, e.UserID)
+	if err != nil {
+		return err
+	}
+	id, err := result.LastInsertId()
+	e.ID = id
+	return err
 }
 
 func GetAllEvents() []Event {
